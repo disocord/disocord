c57ecbff2b32ba289d4cbe07f274744925745386
fsarewfdfwe <72721526+fsarewfdfwe@users.noreply.github.com>
2022-03-03 09:43:49 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from disocord import __version__
+
+setup(
+    name="disocord",
+    license="MIT",
+    version=__version__,
+    description="pycord 踰꾪듉 & 硫붾돱",
+    author="disocord",
+    author_email="disocord@disocord.kro.kr",
+    url="https://github.com/disocord/disocord.paginator",
+    packages=find_packages(),
+    long_description=open('README.md', 'rt', encoding='UTF8').read(),
+    long_description_content_type="text/markdown",
+    keywords=["discord.py", "paginaion", "button", "components", "discord_components","pycord","py-cord"],
+    python_requires=">=3.6",
+    install_requires=["py-cord","pycord-components"],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
